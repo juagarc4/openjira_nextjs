41ac91746afe783f280a91c256926204df8f92fd
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-06 18:01:32 +0200

    Connect to Mongo DB 1

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,16 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  ok: boolean
   name: string
-  method: string
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({
-    ok: true,
     name: 'John Doe',
-    method: req.method || 'No method',
   })
 }
