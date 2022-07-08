e100a07a06683ae029e4ad59ba3a66f70ed30de1
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-08 12:29:21 +0200

    Add entries from frontend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactElement, useReducer, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from 'interfaces'
 import { entriesApi } from 'apis'
@@ -18,14 +17,9 @@ const ENTRIES_INITIAL_STATE: EntriesState = {
 export const EntriesProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE)
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      createdAt: Date.now(),
-      status: 'pending',
-    }
-    dispatch({ type: '[Entry] - Add Entry', payload: newEntry })
+  const addNewEntry = async (description: string) => {
+    const { data } = await entriesApi.post<Entry>('/entries', { description })
+    dispatch({ type: '[Entry] - Add Entry', payload: data })
   }
 
   const updateEntry = (entry: Entry) => {
