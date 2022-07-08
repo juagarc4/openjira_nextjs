7bec815d4a09b72469d02096c25b51572e841e84
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-08 12:21:42 +0200

    Endpoint to add entries

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from 'database'
 import { Entry, IEntry } from 'models'
+import { NewEntry } from '../../../components/ui/NewEntry'
 
 type Data = { message: string } | IEntry[] | IEntry
 
@@ -8,6 +9,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   switch (req.method) {
     case 'GET':
       return getEntries(res)
+    case 'POST':
+      return postEntry(req, res)
     default:
       return res.status(400).json({ message: 'Endpoint does not exist' })
   }
@@ -19,4 +22,26 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect()
 
   res.status(200).json(entries)
+}
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description = '' } = req.body
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  })
+
+  try {
+    await db.connect()
+    await newEntry.save()
+    await db.disconnect()
+    return res.status(201).json(newEntry)
+  } catch (error) {
+    await db.disconnect()
+    console.log(error)
+    res.status(500).json({ message: 'Unepected Error. Please review server console.' })
+  }
+
+  console.log(req.body)
+  res.status(200).json({ message: 'Post working' })
 }
