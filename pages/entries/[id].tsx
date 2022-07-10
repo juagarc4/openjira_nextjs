200277604aceb1bfd5b10c8756e15e0bd3f8c550
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-10 11:01:22 +0200

    Add status options and delete Button

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import { Layout } from 'components/layouts'
-import { Button, Card, CardActions, CardContent, CardHeader, Grid, TextField } from '@mui/material'
+import {
+  capitalize,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  TextField,
+  IconButton,
+} from '@mui/material'
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import { EntryStatus } from 'interfaces'
+
+const validStauts: EntryStatus[] = ['pending', 'in-progress', 'finished']
+
 const EntryPage = () => {
   return (
     <Layout title='....'>
@@ -18,7 +38,14 @@ const EntryPage = () => {
                 multiline
                 label='New Entry'
               />
-              {/* RADIO */}
+              <FormControl>
+                <FormLabel>Status:</FormLabel>
+                <RadioGroup row>
+                  {validStauts.map((option) => (
+                    <FormControlLabel key={option} value={option} control={<Radio />} label={capitalize(option)} />
+                  ))}
+                </RadioGroup>
+              </FormControl>
             </CardContent>
             <CardActions>
               <Button startIcon={<SaveOutlinedIcon />} variant='contained' fullWidth>
@@ -28,6 +55,9 @@ const EntryPage = () => {
           </Card>
         </Grid>
       </Grid>
+      <IconButton sx={{ position: 'fixed', bottom: 30, right: 30, backgroundColor: 'red' }}>
+        <DeleteOutlinedIcon />
+      </IconButton>
     </Layout>
   )
 }
--- themes/dark-theme.ts
+++ themes/dark-theme.ts
@@ -1,13 +1,26 @@
 import { createTheme } from '@mui/material'
-import { red } from '@mui/material/colors'
+import { pink, red } from '@mui/material/colors'
+import Main from 'next/document'
 
+// Allowed items for palette
+// primary
+// secondary
+// error
+// warning
+// info
+// success
+
+// Allowed attributes into each palete item
+//   light?: string;
+//   main: string;
+//   dark?: string;
+//   contrastText?: string;
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     secondary: {
       main: '#19857b',
     },
-
     error: {
       main: red.A400,
     },
